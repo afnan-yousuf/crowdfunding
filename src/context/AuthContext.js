3bc5240377b817c74e827545ff7a8cb8f9351752
afnan-yousuf <afnanyousuf@gmail.com>
2025-03-08 15:29:47 +0500

    authcontext done

---
+++ src/context/AuthContext.js
@@ -0,0 +1,35 @@
+const { createContext, useState, useContext } = require("react");
+const { useNavigate } = require("react-router-dom");
+
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) =>{
+
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    const login = (username,password) =>{
+        if(username == "admin" && password == "123"){
+            setUser(username)
+            localStorage.setItem("user",username)
+            navigate('/dashboard')
+        }
+    }
+
+    const logout = ()=>{
+        setUser(null);
+        localStorage.removeItem("user")
+        navigate("/")
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+          {children}
+        </AuthContext.Provider>
+    );
+
+}
+
+export const useAuth = () =>{
+    return useContext(AuthContext)
+}
